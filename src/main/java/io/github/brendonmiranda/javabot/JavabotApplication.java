f89641f39dcfa33069eee499174c89f62b6f1fc5
brendonfm <freitas.brendon@gmail.com>
2020-04-18 19:45:12 +0100

    initial commit

---
+++ src/main/java/io/github/brendonmiranda/javabot/JavabotApplication.java
@@ -0,0 +1,13 @@
+package io.github.brendonmiranda.javabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/brendonmiranda/javabot/JavabotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brendonmiranda.javabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
