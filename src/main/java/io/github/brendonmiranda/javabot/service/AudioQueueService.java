01c0014b91b70b66e42e4a534a4eca06e6533b50
brendonmiranda <freitas.brendon@gmail.com>
2020-06-20 19:11:36 +0100

    DJB-14 implementing audio queue service

---
+++ src/main/java/io/github/brendonmiranda/javabot/service/AudioQueueService.java
@@ -0,0 +1,48 @@
+package io.github.brendonmiranda.javabot.service;
+
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AudioQueueService {
+
+	@Autowired
+	private RabbitAdmin rabbitAdmin;
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	public void enqueue(String routingKey, AudioTrack audioTrack) {
+		if (hasQueue(routingKey))
+			rabbitTemplate.convertAndSend(routingKey, audioTrack);
+
+		// todo: to implement a else in case of there is no queue
+	}
+
+	/**
+	 * Checks if a queue exists. If the queue doesn't exist it tries to create it.
+	 * @param queueName
+	 * @return
+	 */
+	private boolean hasQueue(String queueName) {
+		// getQueueProperties() can be used to determine if a queue exists on the broker
+		if (rabbitAdmin.getQueueProperties(queueName) == null)
+			return createQueue(queueName) == null ? false : true;
+		else
+			return true;
+	}
+
+	/**
+	 * Creates a queue on the broker and returns the queue name if successful, otherwise
+	 * it returns null.
+	 * @return queueName
+	 */
+	private String createQueue(String queueName) {
+		return rabbitAdmin.declareQueue(new Queue(queueName));
+	}
+
+}
